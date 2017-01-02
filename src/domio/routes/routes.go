e6c95cb0dbfd834562cf6ef7129105a9980eb421
Sergey Basharov <sbasharov@spotware.com>
2017-01-02 10:53:58 +0200

    cleanup

--- src/domio/routes/routes.go
+++ src/domio/routes/routes.go
@@ -1,94 +1,101 @@
 package routes
 
 import (
-	"net/http"
-	"domio/handlers/create_user_handler"
-	"domio/handlers/login_user_handler"
-	"domio/handlers/verify_token_handler"
-	"domio/handlers/get_available_domains_handler"
-	"domio/handlers/get_user_domains_handler"
-	"domio/handlers/create_domain_handler"
-	"domio/handlers/create_rental_handler"
-	"domio/handlers/get_domain_info_handler"
-	"domio/handlers/create_payment_handler"
-	"domio/handlers/create_plan_handler"
-	"domio/handlers/create_subscription_handler"
+    "net/http"
+    "domio/handlers/create_user_handler"
+    "domio/handlers/login_user_handler"
+    "domio/handlers/verify_token_handler"
+    "domio/handlers/get_available_domains_handler"
+    "domio/handlers/get_user_domains_handler"
+    "domio/handlers/create_domain_handler"
+    "domio/handlers/create_rental_handler"
+    "domio/handlers/get_domain_info_handler"
+    "domio/handlers/create_payment_handler"
+    "domio/handlers/create_plan_handler"
+    "domio/handlers/create_subscription_handler"
+    "domio/handlers/create_card_handler"
 )
 
 type Route struct {
-	Name        string
-	Method      string
-	Pattern     string
-	HandlerFunc http.HandlerFunc
+    Name        string
+    Method      string
+    Pattern     string
+    HandlerFunc http.HandlerFunc
 }
 
 type Routes []Route
 
 var RoutesList = Routes{
-	Route{
-		"CreateUser",
-		"POST",
-		"/users",
-		create_user_handler.CreateUserHandler,
-	},
-	Route{
-		"LoginUser",
-		"POST",
-		"/users/login",
-		login_user_handler.LoginUser,
-	},
-	Route{
-		"VerifyToken",
-		"POST",
-		"/tokens/verify",
-		verify_token_handler.VerifyTokenHandler,
-	},
-	Route{
-		"GetAvailableDomains",
-		"GET",
-		"/domains/available",
-		get_available_domains_handler.GetAvailableDomainsHandler,
-	},
-	Route{
-		"GetUserDomains",
-		"GET",
-		"/domains/user",
-		get_user_domains_handler.GetUserDomainsHandler,
-	},
-	Route{
-		"CreateDomain",
-		"POST",
-		"/domains",
-		create_domain_handler.CreateDomainHandler,
-	},
-	Route{
-		"CreateRental",
-		"POST",
-		"/rentals",
-		create_rental_handler.CreateRentalHandler,
-	},
-	Route{
-		"CreatePlan",
-		"POST",
-		"/plans",
-		create_plan_handler.CreatePlanHandler,
-	},
-	Route{
-		"CreateSubscription",
-		"POST",
-		"/subscriptions",
-		create_subscription_handler.CreateSubscriptionHandler,
-	},
-	Route{
-		"CreatePayment",
-		"POST",
-		"/payments",
-		create_payment_handler.CreatePaymentHandler,
-	},
-	Route{
-		"GetDomainInfo",
-		"GET",
-		"/domains/{name}",
-		get_domain_info_handler.GetDomainInfoHandler,
-	},
+    Route{
+        "CreateUser",
+        "POST",
+        "/users",
+        create_user_handler.CreateUserHandler,
+    },
+    Route{
+        "LoginUser",
+        "POST",
+        "/users/login",
+        login_user_handler.LoginUser,
+    },
+    Route{
+        "VerifyToken",
+        "POST",
+        "/tokens/verify",
+        verify_token_handler.VerifyTokenHandler,
+    },
+    Route{
+        "GetAvailableDomains",
+        "GET",
+        "/domains/available",
+        get_available_domains_handler.GetAvailableDomainsHandler,
+    },
+    Route{
+        "GetUserDomains",
+        "GET",
+        "/domains/user",
+        get_user_domains_handler.GetUserDomainsHandler,
+    },
+    Route{
+        "CreateDomain",
+        "POST",
+        "/domains",
+        create_domain_handler.CreateDomainHandler,
+    },
+    Route{
+        "CreateRental",
+        "POST",
+        "/rentals",
+        create_rental_handler.CreateRentalHandler,
+    },
+    Route{
+        "CreatePlan",
+        "POST",
+        "/plans",
+        create_plan_handler.CreatePlanHandler,
+    },
+    Route{
+        "CreateSubscription",
+        "POST",
+        "/subscriptions",
+        create_subscription_handler.CreateSubscriptionHandler,
+    },
+    Route{
+        "CreatePayment",
+        "POST",
+        "/payments",
+        create_payment_handler.CreatePaymentHandler,
+    },
+    Route{
+        "CreateCard",
+        "POST",
+        "/cards",
+        create_card_handler.CreateCardHandler,
+    },
+    Route{
+        "GetDomainInfo",
+        "GET",
+        "/domains/{name}",
+        get_domain_info_handler.GetDomainInfoHandler,
+    },
 }
