d533321507110b536dbfdae44490d702abdd3a26
Sergei Basharov <sergey@basharov.net>
2017-01-04 20:51:24 +0200

    go installing script config

--- install_go.sh
+++ install_go.sh
@@ -2,6 +2,10 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 
+rm -rf /godist
+rm /usr/bin/go
+rm /usr/bin/go-fmt
+
 mkdir /godist
 cd /godist
 wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz
@@ -12,3 +16,5 @@ ln -s /usr/local/go/bin/go /usr/bin/go
 ln -s /usr/local/go/bin/go-fmt /usr/bin/go-fmt
 
 export GOROOT=/usr/local/go
+
+rm go1.7.4.linux-amd64.tar.gz
