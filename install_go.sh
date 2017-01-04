acd568b6bc8b6b461094caa7433f5a0b3d999a51
Sergei Basharov <sergey@basharov.net>
2017-01-04 20:49:37 +0200

    added go installing script

---
+++ install_go.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get -y upgrade
+
+mkdir /godist
+cd /godist
+wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz
+sudo tar -xvf go1.7.4.linux-amd64.tar.gz
+sudo mv go /usr/local
+
+ln -s /usr/local/go/bin/go /usr/bin/go
+ln -s /usr/local/go/bin/go-fmt /usr/bin/go-fmt
+
+export GOROOT=/usr/local/go
