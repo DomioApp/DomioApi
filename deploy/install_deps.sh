d35b2b5fb562f823035e413ae1fab8476e31424b
Sergey Basharov <sbasharov@spotware.com>
2017-01-05 17:45:24 +0200

    'scripts'

--- deploy/install_deps.sh
+++ deploy/install_deps.sh
@@ -6,6 +6,7 @@ go get -u github.com/go-ini/ini
 go get -u github.com/jmespath/go-jmespath
 go get -u github.com/fatih/color
 go get -u github.com/gorilla/mux
+go get -u github.com/gorilla/context
 go get -u github.com/jmoiron/sqlx
 go get -u github.com/lib/pq
 go get -u github.com/stripe/stripe-go
