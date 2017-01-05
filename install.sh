45a02522318844682f6dec75df0ebbda8e7361ad
Sergey Basharov <sbasharov@spotware.com>
2017-01-05 16:58:31 +0200

    install scripts

--- install.sh
+++ install.sh
@@ -1,13 +1,4 @@
 export GOPATH=$PWD
 echo $GOPATH
 
-go get -u github.com/dgrijalva/jwt-go
-go get -u github.com/aws/aws-sdk-go/aws
-go get -u github.com/go-ini/ini
-go get -u github.com/jmespath/go-jmespath
-go get -u github.com/fatih/color
-go get -u github.com/gorilla/mux
-go get -u github.com/jmoiron/sqlx
-go get -u github.com/lib/pq
-go get -u github.com/stripe/stripe-go
-go get -u golang.org/x/crypto/bcrypt
+sh deploy/install_deps.sh
