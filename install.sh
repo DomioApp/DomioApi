c81817efc3eb283c96536d6ef6e21af65419030c
Sergei Basharov <sergey@basharov.net>
2017-01-06 19:17:16 +0200

    deploy config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#sh ~/domioapi/deploy/apt_update.sh
+sh ~/domioapi/deploy/apt_update.sh
 
 if ! [ -x "$(command -v go)" ]; then
    echo 'go is not installed.' >&2
@@ -9,10 +9,13 @@ if ! [ -x "$(command -v go)" ]; then
    echo "Go is already installed!" >&2
 fi
 
-cat ~/domioapi/deploy/build.sh
+sh ~/domioapi/deploy/install_deps.sh
 
-#sh ~/domioapi/deploy/install_deps.sh
+if ! [ -x "$(command -v psql)" ]; then
+   echo 'Postgres is not installed.' >&2
+   sh ~/domioapi/deploy/install_pg.sh
+  else
+   echo "Postgres is already installed!" >&2
+fi
 
 sh ~/domioapi/deploy/build.sh
-
-#sh ~/domioapi/deploy/install_pg.sh
