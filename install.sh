2225cb9f2cd58926c953e67e5fbadb1923aa29c3
Sergei Basharov <sergey@basharov.net>
2017-01-06 19:10:09 +0200

    deploy config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sh ~/domioapi/deploy/apt_update.sh
+#sh ~/domioapi/deploy/apt_update.sh
 
 if ! [ -x "$(command -v go)" ]; then
    echo 'go is not installed.' >&2
@@ -12,15 +12,6 @@ fi
 
 #sh ~/domioapi/deploy/install_deps.sh
 
-echo ======================================================
-echo ~/
-echo ======================================================
-
-echo ------------------------------------------------------
-echo ~/domioapi/
-ls ~/domioapi/
-cat ~/domioapi/deploy/buld.sh
-echo ------------------------------------------------------
 sh ~/domioapi/deploy/buld.sh
 
-sh ~/domioapi/deploy/install_pg.sh
+#sh ~/domioapi/deploy/install_pg.sh
