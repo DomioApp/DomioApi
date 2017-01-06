517c6159cc55fd5aa50d90431734358f7a9e5b17
Sergei Basharov <sergey@basharov.net>
2017-01-06 11:06:43 +0200

    build process config

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ sh ~/domioapi/deploy/apt_update.sh
 sh ~/domioapi/deploy/install_go.sh
 #sh deploy/install_gb.sh
 sh ~/domioapi/deploy/install_deps.sh
+echo ======================================================
+echo ~/
+echo ======================================================
 sh ~/domioapi/deploy/buld.sh
 
 sh ~/domioapi/deploy/install_pg.sh
