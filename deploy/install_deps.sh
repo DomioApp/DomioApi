3c60c734965ca9f113d32b07519c79db9430426e
Sergei Basharov <sergey@basharov.net>
2017-01-06 19:25:37 +0200

    deploy config

--- deploy/install_deps.sh
+++ deploy/install_deps.sh
@@ -3,16 +3,16 @@
 echo Installing Go dependendcies...
 cd ~/domioapi
 
-go get -u github.com/dgrijalva/jwt-go
-go get -u github.com/aws/aws-sdk-go/aws
-go get -u github.com/go-ini/ini
-go get -u github.com/jmespath/go-jmespath
-go get -u github.com/fatih/color
-go get -u github.com/gorilla/mux
-go get -u github.com/gorilla/context
-go get -u github.com/jmoiron/sqlx
-go get -u github.com/lib/pq
-go get -u github.com/stripe/stripe-go
-go get -u golang.org/x/crypto/bcrypt
+go get -u -v github.com/dgrijalva/jwt-go
+go get -u -v github.com/aws/aws-sdk-go/aws
+go get -u -v github.com/go-ini/ini
+go get -u -v github.com/jmespath/go-jmespath
+go get -u -v github.com/fatih/color
+go get -u -v github.com/gorilla/mux
+go get -u -v github.com/gorilla/context
+go get -u -v github.com/jmoiron/sqlx
+go get -u -v github.com/lib/pq
+go get -u -v github.com/stripe/stripe-go
+go get -u -v golang.org/x/crypto/bcrypt
 
 echo Go dependencies installed!
