b2e7f7ccb646393f9fd05318c2afbb9b5ccf4dda
Sergei Basharov <sergey@basharov.net>
2017-01-06 11:02:04 +0200

    build process config

--- install.sh
+++ install.sh
@@ -2,25 +2,10 @@
 
 set -e
 
-sh deploy/apt_update.sh
-sh deploy/install_go.sh
+sh ~/domioapi/deploy/apt_update.sh
+sh ~/domioapi/deploy/install_go.sh
 #sh deploy/install_gb.sh
-sh deploy/install_deps.sh
-#sh deploy/buld.sh
+sh ~/domioapi/deploy/install_deps.sh
+sh ~/domioapi/deploy/buld.sh
 
-
-echo Building Domio...
-rm -rf /domio
-mkdir /domio
-
-cd ~/domioapi
-export GOPATH=$PWD
-echo $GOPATH
-
-go build -o /domio/domio domio
-
-cd /
-rm -rf ~/domioapi
-
-
-sh deploy/install_pg.sh
+sh ~/domioapi/deploy/install_pg.sh
