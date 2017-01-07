194dba9d1fa30361381eccb40c538c0681af72b4
Sergei Basharov <sergey@basharov.net>
2017-01-07 13:35:43 +0200

    [ci skip] moved config out of env variables to a json file

--- deploy/config/domio_service.sh
+++ deploy/config/domio_service.sh
@@ -19,23 +19,25 @@ set -e
 
 case "$1" in
  start)
- echo -----------------------------------
- echo "PIDFILE: ${PIDFILE}"
- echo "NAME: ${NAME}"
- echo -n "Starting ${DESC}: "
- echo -----------------------------------
- start-stop-daemon $START_OPTS >> $LOGFILE
+# echo -----------------------------------
+# echo "PIDFILE: ${PIDFILE}"
+# echo "NAME: ${NAME}"
+# echo -n "Starting ${DESC}: "
+# echo -----------------------------------
+# start-stop-daemon $START_OPTS >> $LOGFILE
  echo "$NAME."
+ start-stop-daemon --start --background --make-pidfile --pidfile /var/run/domio.pid --exec /domio/domio
  ;;
 stop)
- echo -----------------------------------
- echo "PIDFILE: ${PIDFILE}"
- echo "NAME: ${NAME}"
- echo -n "Stopping $DESC: "
- echo -----------------------------------
- start-stop-daemon $STOP_OPTS
- echo "$NAME."
- rm -f $PIDFILE
+# echo -----------------------------------
+# echo "PIDFILE: ${PIDFILE}"
+# echo "NAME: ${NAME}"
+# echo -n "Stopping $DESC: "
+# echo -----------------------------------
+# start-stop-daemon $STOP_OPTS
+# echo "$NAME."
+# rm -f $PIDFILE
+ start-stop-daemon --stop --pidfile /var/run/domio.pid
  ;;
 restart|force-reload)
  echo -n "Restarting $DESC: "
