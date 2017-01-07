8ec89a38e327da5697bf13c361cbbc965d371427
Sergei Basharov <sergey@basharov.net>
2017-01-07 10:06:06 +0200

    configuring domio as a linux service

--- deploy/copy_domio_service_config.sh
+++ deploy/copy_domio_service_config.sh
@@ -2,8 +2,7 @@
 set -e
 
 echo Copying Domio Service Config...
-rm /etc/init.d/domio_service
 
-cp ~/domioapi/deploy/config/domio_service.sh /etc/init.d/domio
+yes | cp -rf ~/domioapi/deploy/config/domio_service.sh /etc/init.d/domio
 
 echo Swagger copied!
