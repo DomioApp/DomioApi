2a1196826dabc7893aa8282589f1e73fff02b8a2
Sergei Basharov <sergey@basharov.net>
2017-01-08 18:48:49 +0200

    added public site config

--- deploy/copy_public_site.sh
+++ deploy/copy_public_site.sh
@@ -3,7 +3,7 @@ set -e
 
 echo Copying public site files...
 
-rm -rf /usr/share/nginx/html/public
-yes | mv -f ~/domioapi/public /usr/share/nginx/html
+rm -rf /usr/share/nginx/html/public_site
+yes | mv -f ~/domioapi/public_site /usr/share/nginx/html
 
 echo Public site files copied!
