c2199880af79327df376429d483a8c30cea2b6fc
Sergei Basharov <sergey@basharov.net>
2017-01-08 12:12:22 +0200

    [ci skip] nginx config

--- src/domio/main.go
+++ src/domio/main.go
@@ -16,12 +16,11 @@ func init() {
 }
 
 func main() {
-    port := Config.PORT
     domiorouter := router.NewRouter()
-    log.Printf("Web server is running on http://localhost:%d", port)
+    log.Printf("Web server is running on http://localhost:%d", Config.PORT)
     err := http.ListenAndServe(":8080", domiorouter)
     if (err != nil) {
-        msg := fmt.Sprintf("Failed to start web server on port %d", port)
+        msg := fmt.Sprintf("Failed to start web server on port %d", Config.PORT)
         log.Fatal(msg)
     }
 }
