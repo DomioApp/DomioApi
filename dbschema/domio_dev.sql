99f6bf8010e4de856925fd4e3130bf9b9d9faa23
Sergey Basharov <sbasharov@spotware.com>
2017-01-09 16:13:35 +0200

    added role field to user

--- dbschema/domio_dev.sql
+++ dbschema/domio_dev.sql
@@ -2,11 +2,12 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.5.5
--- Dumped by pg_dump version 9.5.5
+-- Dumped from database version 9.6.1
+-- Dumped by pg_dump version 9.6.1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
@@ -60,12 +61,13 @@ ALTER TABLE ONLY domains ALTER COLUMN rented_by SET STORAGE PLAIN;
 CREATE TABLE users (
     email character varying(30) NOT NULL,
     password text,
-    id character(18)
+    id character(18),
+    role character varying(10) NOT NULL
 );
 
 
 --
--- Name: domains_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: domains domains_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY domains
@@ -73,7 +75,7 @@ ALTER TABLE ONLY domains
 
 
 --
--- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY users
@@ -95,7 +97,7 @@ CREATE INDEX fki_rented_by ON domains USING btree (rented_by);
 
 
 --
--- Name: owner; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: domains owner; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY domains
@@ -103,7 +105,7 @@ ALTER TABLE ONLY domains
 
 
 --
--- Name: rented_by; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: domains rented_by; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY domains
@@ -114,10 +116,7 @@ ALTER TABLE ONLY domains
 -- Name: public; Type: ACL; Schema: -; Owner: -
 --
 
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM sergeibasharov;
 GRANT ALL ON SCHEMA public TO sergeibasharov;
-GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --
