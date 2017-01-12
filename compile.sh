b3326ea0ad002618f91e01f68fed03165b7ac0f1
Sergei Basharov <sbasharov@ctrader.local>
2017-01-12 14:42:37 +0000

    added name generating for tags

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-go build -o bin/domio.exe -ldflags "-X main.Buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.Version=`git rev-parse --short HEAD`" domio
+go build -o bin/domio.exe -ldflags "-X main.Buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.Hash=`git rev-parse --short HEAD`  -X main.Version=`git tag -l --points-at HEAD`" domio
