a1e518145ecfc0b20b471bc4bb719dc4b3dc93f0
Sergey Basharov <sbasharov@spotware.com>
2017-01-13 16:30:15 +0200

    Formatting

--- compile.sh
+++ compile.sh
@@ -5,18 +5,21 @@ buildstamp=`date -u '+%s'`
 hash=`git rev-parse --short HEAD`
 version=`git tag -l --points-at HEAD`
 
-echo ------------------------------------------------------
-echo "Buildstamp: ${buildstamp}"
-echo "Hash:       ${hash}"
-echo "Version:    ${version}"
-echo ------------------------------------------------------
+echo
+echo ---------------------------
+echo "  Buildstamp: ${buildstamp}"
+echo "  Hash:       ${hash}"
+echo "  Version:    ${version}"
+echo ---------------------------
+
+echo
+echo Compiling for Windows...
+set GOARCH=amd64
+set GOOS=windows
+go build -o /usr/local/bin/domio.exe -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" domio
+
 
 echo Compiling for Linux...
 set GOARCH=amd64
 set GOOS=linux
 go build -o /usr/local/bin/domio -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" domio
-
-echo Compiling for Windows...
-set GOARCH=amd64
-set GOOS=windows
-go build -o /usr/local/bin/domio.exe -ldflags "-X main.Buildstamp=$buildstamp -X main.Hash=$hash  -X main.Version=$version" domio
