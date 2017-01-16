b247d61a64e5a434db5196c5fd50f9f5919ce715
Sergei Basharov <sergey@basharov.net>
2017-01-16 19:54:03 +0200

    renamed app name for logger

--- src/domio_api/components/logger/logger.go
+++ src/domio_api/components/logger/logger.go
@@ -9,7 +9,7 @@ var Logger *syslog.Writer
 
 func init() {
     var err error
-    Logger, err = syslog.Dial("udp", "logs5.papertrailapp.com:18422", syslog.LOG_EMERG | syslog.LOG_KERN, "domio")
+    Logger, err = syslog.Dial("udp", "logs5.papertrailapp.com:18422", syslog.LOG_EMERG | syslog.LOG_KERN, "domio-api")
     if err != nil {
         log.Fatal("failed to dial syslog")
     }
