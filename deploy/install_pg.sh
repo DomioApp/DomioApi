b573f29213ba5005ad05f8b4d6f8190c1a809990
Sergei Basharov <sergey@basharov.net>
2017-01-22 12:42:50 +0200

    Update postgres to 9.6.1

--- deploy/install_pg.sh
+++ deploy/install_pg.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 echo Installing Postgres...
-sudo apt-get -y install postgresql postgresql-contrib
+sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y postgresql-9.6
 psql --version
