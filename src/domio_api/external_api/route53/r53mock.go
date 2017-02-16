d75b71fa3e3321d4c1a986bfd1a438aa3f8f2557
Sergey Basharov <sbasharov@spotware.com>
2017-02-16 15:20:16 +0200

    [skip ci] mocking r53

--- src/domio_api/external_api/route53/r53mock.go
+++ src/domio_api/external_api/route53/r53mock.go
@@ -3,14 +3,7 @@ package route53
 import (
     "domio_api/db"
     "github.com/aws/aws-sdk-go/service/route53"
-    "domio_api/components/config"
-    "github.com/aws/aws-sdk-go/aws/credentials"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/fatih/color"
-    "fmt"
-    "log"
-    "time"
+    "github.com/aws/aws-sdk-go/service/route53domains"
 )
 
 func DeleteDomainZoneMock(domain *domiodb.Domain) error {
@@ -18,31 +11,14 @@ func DeleteDomainZoneMock(domain *domiodb.Domain) error {
 }
 
 func CreateDomainZoneMock(domain *domiodb.Domain) (*route53.CreateHostedZoneOutput, error) {
-    conf := config.Config
-    token := ""
-    creds := credentials.NewStaticCredentials(conf.AWS_ACCESS_KEY_ID, conf.AWS_SECRET_ACCESS_KEY, token)
-    sess, err := session.NewSession(&aws.Config{Credentials: creds})
-    if err != nil {
-        fmt.Println("failed to create session,", err)
-        return &route53.CreateHostedZoneOutput{}, err
-    }
+    resp := route53.CreateHostedZoneOutput{}
 
-    r53Service := route53.New(sess)
-    id := time.Now().Format(time.RFC850);
-
-    params := &route53.CreateHostedZoneInput{
-        CallerReference: &id,
-        Name:            aws.String(domain.Name),
-    }
-    resp, err := r53Service.CreateHostedZone(params)
-
-    if err != nil {
-        color.Set(color.FgRed)
-        log.Println(params.CallerReference)
-        log.Println(id)
-        log.Println(err)
-        color.Unset()
-        return nil, err
+    resp.HostedZone.Id = "hosted_zone_id"
+    resp.DelegationSet.NameServers = &[]route53domains.Nameserver{
+        *route53domains.Nameserver{Name:"1"},
+        *route53domains.Nameserver{Name:"2"},
+        *route53domains.Nameserver{Name:"3"},
+        *route53domains.Nameserver{Name:"4"},
     }
 
     return resp, nil
