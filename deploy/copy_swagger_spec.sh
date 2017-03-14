f75f52bed1d94a6ee26356cb023165554765a641
Sergei Basharov <sergey@basharov.net>
2017-03-14 07:41:12 +0200

    swagger schema path fix

--- deploy/copy_swagger_spec.sh
+++ deploy/copy_swagger_spec.sh
@@ -3,6 +3,6 @@ set -e
 
 echo Copying Swagger Domio Spec...
 
-yes | mv -f ~/domioapi/swagger_schema.json /usr/local/domio_api
+yes | mv -f ~/domioapi/swagger_schema/domio_api.json /usr/local/domio_api
 
 echo Swagger Domio Spec copied!
